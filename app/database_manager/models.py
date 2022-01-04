ef268d3589ab83c91663354cd751b0ea75fbcf57
nihankarslioglu <nihannkarslioglu@gmail.com>
2022-01-04 11:47:34 -0800

    signup code is cleaned. Some error checks are added.

---
+++ app/database_manager/models.py
@@ -0,0 +1,25 @@
+from app import db
+
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(250), nullable=False)
+    fullname = db.Column(db.String(250), nullable=False)
+    password = db.Column(db.String(250), nullable=False)
+    mobile_phone = db.Column(db.Integer, nullable=False)
+    bank_info = db.relationship('BankAccount', backref='user', lazy=True,  uselist=False)
+
+
+class BankAccount(db.Model):
+    bank_id = db.Column(db.Integer, primary_key=True)
+    amount = db.Column(db.Integer)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    bank_number = db.Column(db.Integer)
+
+
+def init_db():
+    db.create_all()
+
+
+if __name__ == '__main__':
+    init_db()
--- app/main.py
+++ app/main.py
@@ -1,9 +1,14 @@
-import base64
-
 from flask import render_template, request, session, redirect
 
 from app import db, app, bcrypt
-from models import User
+from app.database_manager.models import User, BankAccount
+from app.managers.person_manager import FormParametersForPerson, Person, register_person
+
+# source venv/bin/activate
+# run Flask application from terminal
+# export FLASK_APP=main.py
+# flask run
+
 
 @app.route('/')
 def index():
@@ -11,38 +16,24 @@ def index():
     return render_template('index.html')
 
 
-# source venv/bin/activate
-# run Flask application from terminal
-# export FLASK_APP=main.py
-# flask run
 @app.route('/userSignup', methods=['GET', 'POST'])
 def signup_user():
     if request.method == 'POST':
-        username = request.form['username']
-        password = request.form['password']
-        password_hash = bcrypt.generate_password_hash(password, 10)
+        if request.form.get(FormParametersForPerson.username.name) is not None \
+                and request.form.get(FormParametersForPerson.password.name) is not None\
+                and request.form.get(FormParametersForPerson.fullname.name) is not None \
+                and request.form.get(FormParametersForPerson.mobile_phone.name) is not None \
+                and request.form.get(FormParametersForPerson.bank_number.name) is not None:
 
-        firstname = request.form['firstname']
-        lastname = request.form['lastname']
-        url = request.form['url']
-        mobile_phone = request.form['mobile_phone']
+            username = request.form[FormParametersForPerson.username.name]
+            password = request.form[FormParametersForPerson.password.name]
+            fullname = request.form[FormParametersForPerson.fullname.name]
+            mobile_phone = request.form[FormParametersForPerson.mobile_phone.name]
+            bank_number = request.form[FormParametersForPerson.bank_number.name]
 
-        age = request.form['age']
-        description = request.form['description']
-        bank_field = request.form['bank_field']
-
-        print(username, password, firstname, lastname, url, mobile_phone, age, description, bank_field)
-
-        user = User(username=username, password=password_hash, firstname=firstname,
-                    lastname=lastname, url=url, mobile_phone=mobile_phone, age=age, description=description,
-                    bank_field=bank_field)
-
-        s = db.session()
-        s.add(user)
-        s.commit()
-        message = "New user is created!"
+            person = Person(username, password, fullname, mobile_phone, bank_number)
+            message = register_person(person)
         return render_template('index.html', message=message)
-
     else:
         return render_template('index.html')
 
@@ -66,11 +57,12 @@ def login():
         if conn_success:
             message = 'connection is successful'
             session['username'] = uname
-            return user_info()
+            return user_info(result.id, uname)
         else:
             return render_template('login.html', message=message)
     else:
         return render_template('login.html')
+
 
 def get_user_info(username):
     s = db.session()
@@ -78,21 +70,79 @@ def get_user_info(username):
     return result
 
 
-@app.route('/userInfo', methods=['GET', 'POST'])
-def user_info():
-    if 'username' in session:
-        username = session['username']
-        user = get_user_info(username)
+def get_user_info_byUserId(person_id):
+    s = db.session()
+    result = User.query.filter_by(id=person_id).first()
+    return result
+
+
+def get_bank_info_byUserId(person_id):
+    s = db.session()
+    result = BankAccount.query.filter_by(user_id=person_id).first()
+    return result
+
+
+@app.route('/donate/<person_id>', methods=['GET', 'POST'])
+def user_info(person_id):
+    user = get_user_info_byUserId(person_id)
+    if user is not None:
         return render_template('paymentPage.html', **userToDict(user))
+    else:
+        return render_template('404.html')
+
+
+
+@app.route('/userInfo_for_market/<person_id>', methods=['GET', 'POST'])
+def user_info_for_market(person_id):
+    print(person_id)
+    user = get_user_info_byUserId(person_id)
+    print(user.username)
+
+    # return render_template('paymentPage.html', **userToDict(user))
+
+
+@app.route('/requestPayment/<person_id>/<amount>', methods=['GET', 'POST'])
+def requestPayment(person_id, amount):
+    print(person_id + " " + amount)
+
+    user = get_user_info_byUserId(person_id)
+    bank_amount = get_bank_info_byUserId(person_id)
+
+    amount = int(amount)
+    if(bank_amount.amount >= amount): # there is enough money
+        message = "transaction is successful for amount of payment " + str(amount) + "\n"
+        bank_amount.amount = bank_amount.amount - amount
+        db.session().commit()
+    else:
+        message = "transaction failed for amount of donation " + str(amount)
+
+    return render_template("authorized.txt", username=user.username, amount=amount, message=message, remaining_money=bank_amount.amount)
+    # return render_template('paymentPage.html', **userToDict(user), message=message)
+
+
+@app.route('/donate2/<person_id>', methods=['GET', 'POST'])
+def donate(person_id):
+    if request.method == 'POST':
+        user = get_user_info_byUserId(person_id)
+        amount = request.form['amountInput']
+        print(person_id + " " + amount)
+        message = "Transaction is successful for amount of donation " + amount
+        # make transaction in db
+        bank_info = get_bank_info_byUserId(person_id)
+        old_amount = bank_info.amount
+        bank_info.amount = old_amount + int(amount)
+        db.session().commit()
+        return render_template('paymentPage.html', **userToDict(user), message=message)
+
 
 def userToDict(user):
     d = {}
     d['username'] = user.username
-    d['firstname'] = user.firstname
-    d['lastname'] = user.lastname
-    d['url'] = user.url
+    d['fullname'] = user.fullname
     d['mobile_phone'] = user.mobile_phone
-    d['description'] = user.description
+    d['person_id'] = user.id
+    bank_account = user.bank_info
+    d['curr_amount'] = bank_account.amount
     return d
 
 @app.route('/editUserInfo', methods=['GET', 'POST'])
@@ -150,4 +200,5 @@ def listUsers():
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    # host u ekleki herkes ulassin
+    app.run(debug=True, host='0.0.0.0')
