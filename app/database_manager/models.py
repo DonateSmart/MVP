b06540ed83b7dd4eb5fdc872a93b2b905b1e4017
iamvarshith <varshithmuthya.vankamamidi18@st.niituniversity.in>
2022-01-30 17:04:02 +0530

    Update 8/01/22

--- app/database_manager/models.py
+++ app/database_manager/models.py
@@ -1,23 +1,16 @@
 from app import db
 
 
-class User(db.Model):
+class HLPerson(db.Model):
     id = db.Column(db.Integer, primary_key=True)
+    donate_smart_id = db.Column(db.String(250), nullable=False)
     username = db.Column(db.String(250), nullable=False)
-
     password = db.Column(db.String(250), nullable=False)
-
     url = db.Column(db.String(250), nullable=False)
+    bank_field = db.Column(db.String(250), nullable=False)
     description = db.Column(db.String(250), nullable=False)
     age = db.Column(db.Integer, nullable=False)
-    bank_info = db.relationship('BankAccount', backref='user', lazy=True,  uselist=False)
-
-
-class BankAccount(db.Model):
-    bank_id = db.Column(db.Integer, primary_key=True)
-    amount = db.Column(db.Integer)
-    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
-    bank_number = db.Column(db.Integer)
+    image_url = db.Column(db.String(250), nullable=False)
 
 
 def init_db():
